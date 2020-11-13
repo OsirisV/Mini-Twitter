f433dadac15cc3ff64593e62716d884f8473ed3d
Osiris Sielatshom <Osirissiel@yahoo.fr>
2020-11-13 00:15:33 -0800

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,18 @@
+/*
+ * Osiris Sielatshom
+ * CS 3560
+ * ASSISGNMENT #2
+ * Driver Class with main method
+ */
+package main;
+
+/**
+ *
+ * @author admin
+ */
+public class Driver {
+    
+    public static void main(String[] args) {
+        AdminControlPanel.getInstance().setVisible(true);
+    }
+}
